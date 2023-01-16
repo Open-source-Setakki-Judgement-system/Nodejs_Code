820a4b62a889328944f108e7687523d45037781c
phoenix9469 <phoenix9469@kakao.com>
2023-01-16 21:55:41 +0900

    Multicast Test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const dbsettings = require('./var.js');
 const express = require('express')
 const cors = require('cors')
 var mysql = require('mysql');
-const admin = require('firebase-admin')
+const fcm = require('firebase-admin')
 const app = express()
 app.use(cors())
 const http = require('http').createServer(app)
@@ -12,8 +12,8 @@ const gateway = io.of('/gateway');
 let serAccount = require('./firebase_token.json')
 const port = 1900
 
-admin.initializeApp({
-    credential: admin.credential.cert(serAccount),
+fcm.initializeApp({
+    credential: fcm.credential.cert(serAccount),
 })
 
 var connection = mysql.createConnection({
@@ -34,16 +34,27 @@ app.get('/', (req, res) => {
 })
 
 app.get('/hi', (req, res) => {
-    let target_token =
-        'c_cffKHoRcGhXqYB7uNiXZ:APA91bFqiO_e_Or_6HR9iZeU6grfidkU25YDA2mcty1chchsW_42u3MRMiRGH6YEoYt4iulhYJM3xvrmiZPEdOZbHDxyofdq8hRnBut3ztsVYSqHwcdPzr-5i2ePgAVw9Gafs_G7sn59'
-    //target_token은 푸시 메시지를 받을 디바이스의 토큰값입니다
+    var target_tokens = new Array();
+    connection.query(`SELECT Token FROM PushAlert;`, function (error, results, fields) {
+        if (error) {
+            console.log(error);
+        }
+        console.log(results);
+        console.log(results.length);
+        for (var i = 0; i < results.length; i++) {
+            console.log(results[i].Token);
+            tokens[i]=results[i].Token;
+        }
+    });
+    // let target_token = ["c_cffKHoRcGhXqYB7uNiXZ:APA91bFqiO_e_Or_6HR9iZeU6grfidkU25YDA2mcty1chchsW_42u3MRMiRGH6YEoYt4iulhYJM3xvrmiZPEdOZbHDxyofdq8hRnBut3ztsVYSqHwcdPzr-5i2ePgAVw9Gafs_G7sn59",]
+    // //target_token은 푸시 메시지를 받을 디바이스의 토큰값입니다
 
     let message = {
         notification: {
             title: '일해라',
-            body: 'IOS 만들어라',
+            body: 'Multicast테스트',
         },
-        token: target_token,
+        tokens: target_tokens,
         android: {
             priority: "high"
         },
@@ -56,15 +67,29 @@ app.get('/hi', (req, res) => {
         }
     }
 
-    admin
-        .messaging()
-        .send(message)
-        .then(function (response) {
-            console.log('Successfully sent message: : ', response)
-        })
-        .catch(function (err) {
-            console.log('Error Sending message!!! : ', err)
-        })
+    // fcm
+    //     .messaging()
+    //     .send(message)
+    //     .then(function (response) {
+    //         console.log('Successfully sent message: : ', response)
+    //     })
+    //     .catch(function (err) {
+    //         console.log('Error Sending message!!! : ', err)
+    //     })
+    fcm.messaging().sendMulticast(message)
+        .then((response) => {
+            if (response.failureCount > 0) {
+                const failedTokens = [];
+                response.responses.forEach((resp, idx) => {
+                    if (!resp.success) {
+                        failedTokens.push(deviceToken[idx]);
+                    }
+                });
+                console.log('List of tokens that caused failures: ' + failedTokens);
+            }
+            console.log('success')
+            return res.status(200).json({success: true})
+        });
 })
 
 application.on('connection', socket => {
@@ -97,6 +122,7 @@ application.on('connection', socket => {
 gateway.on('connection', socket => {
     console.log('connected', socket.id)
 
+
     socket.on('update_state', state_data => {
         console.log(state_data)
         const stateJSON = JSON.stringify(state_data)
@@ -104,6 +130,13 @@ gateway.on('connection', socket => {
         console.log(parsedstate.id)
         console.log(parsedstate.state)
         connection.query(`UPDATE deviceStatus SET state = ${parsedstate.state} WHERE id =${parsedstate.id};`, function (error, results, fields) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(results);
+        });
+        application.emit('update', state_data)
+        connection.query(`SELECT Token FROM PushAlert WHERE device_id = ${parsedstate.id} AND Expect_Status = ${parsedstate.state};`, function (error, results, fields) {
             if (error) {
                 console.log(error);
             }
