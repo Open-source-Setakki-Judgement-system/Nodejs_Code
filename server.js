7f666542d34395c427ed63d6689717556ee08f47
phoenix9469 <phoenix9469@kakao.com>
2023-01-16 20:16:25 +0900

    room

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ const app = express()
 app.use(cors())
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const application = io.of('/application');
+const gateway = io.of('/gateway');
 let serAccount = require('./firebase_token.json')
 const port = 1900
 
@@ -33,39 +35,66 @@ app.get('/', (req, res) => {
 
 app.get('/hi', (req, res) => {
     let target_token =
-    'c_cffKHoRcGhXqYB7uNiXZ:APA91bFqiO_e_Or_6HR9iZeU6grfidkU25YDA2mcty1chchsW_42u3MRMiRGH6YEoYt4iulhYJM3xvrmiZPEdOZbHDxyofdq8hRnBut3ztsVYSqHwcdPzr-5i2ePgAVw9Gafs_G7sn59'
-	//target_token은 푸시 메시지를 받을 디바이스의 토큰값입니다
+        'c_cffKHoRcGhXqYB7uNiXZ:APA91bFqiO_e_Or_6HR9iZeU6grfidkU25YDA2mcty1chchsW_42u3MRMiRGH6YEoYt4iulhYJM3xvrmiZPEdOZbHDxyofdq8hRnBut3ztsVYSqHwcdPzr-5i2ePgAVw9Gafs_G7sn59'
+    //target_token은 푸시 메시지를 받을 디바이스의 토큰값입니다
 
-  let message = {
-    notification: {
-      title: '일해라',
-      body: 'IOS 만들어라',
-    },
-    token: target_token,
-    android: {
-        priority: "high"
-    },
-    apns: {
-        payload: {
-            aps: {
-                contentAvailable: true,
+    let message = {
+        notification: {
+            title: '일해라',
+            body: 'IOS 만들어라',
+        },
+        token: target_token,
+        android: {
+            priority: "high"
+        },
+        apns: {
+            payload: {
+                aps: {
+                    contentAvailable: true,
+                }
             }
         }
     }
-  }
 
-  admin
-    .messaging()
-    .send(message)
-    .then(function (response) {
-      console.log('Successfully sent message: : ', response)
+    admin
+        .messaging()
+        .send(message)
+        .then(function (response) {
+            console.log('Successfully sent message: : ', response)
+        })
+        .catch(function (err) {
+            console.log('Error Sending message!!! : ', err)
+        })
+})
+
+application.on('connection', socket => {
+    console.log('connected', socket.id)
+
+    socket.on('test', test => {
+        console.log('application')
+        console.log(test)
+        application.emit('msg', 'Halo')
+        application.emit('msg', test)
     })
-    .catch(function (err) {
-      console.log('Error Sending message!!! : ', err)
+
+    socket.on('request_push', push_data => {
+        console.log(push_data)
+        const pushJSON = JSON.stringify(push_data)
+        const parsedpush = JSON.parse(pushJSON)
+        console.log(parsedpush.token)
+        console.log(parsedpush.device_id)
+        console.log(parsedpush.expect_state)
+        connection.query(`INSERT INTO PushAlert (Token, device_id, Expect_Status) VALUES (${parsedpush.token},${parsedpush.device_id},${parsedpush.expect_state})`, function (error, results, fields) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(results);
+        });
+        //io.emit('msg', 'Halo')
     })
 })
 
-io.on('connection', socket => {
+gateway.on('connection', socket => {
     console.log('connected', socket.id)
 
     socket.on('update_state', state_data => {
@@ -84,24 +113,10 @@ io.on('connection', socket => {
     })
 
     socket.on('test', test => {
+        console.log('gateway')
         console.log(test)
-        io.emit('msg', 'Halo')
-        io.emit('msg', test)
+        gateway.emit('msg', 'Halo')
+        gateway.emit('msg', test)
     })
 
-    socket.on('request_push', push_data => {
-        console.log(push_data)
-        const pushJSON = JSON.stringify(push_data)
-        const parsedpush = JSON.parse(pushJSON)
-        console.log(parsedpush.token)
-        console.log(parsedpush.device_id)
-        console.log(parsedpush.expect_state)
-        connection.query(`INSERT INTO PushAlert (Token, device_id, Expect_Status) VALUES (${parsedpush.token},${parsedpush.device_id},${parsedpush.expect_state})`, function (error, results, fields) {
-            if (error) {
-                console.log(error);
-            }
-            console.log(results);
-        });
-        //io.emit('msg', 'Halo')
-    })
 })
