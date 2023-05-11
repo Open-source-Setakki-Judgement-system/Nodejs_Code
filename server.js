29b2fd7f13dbf5dcd9d24b68fdbd2264c8c2443e
phoenix9469 <phoenix9469@kakao.com>
2023-05-11 09:57:15 +0900

    푸시 신청 목록 오름차순 정렬

--- server.js
+++ server.js
@@ -221,7 +221,7 @@ application.on('connection', socket => {
         console.log('Device Token:')
         console.log(token)
 
-        connection.query(`SELECT device_id FROM PushAlert WHERE Token = ?;`, [token], function (error, results) {
+        connection.query(`SELECT device_id FROM PushAlert WHERE Token = ? ORDER BY device_id;`, [token], function (error, results) {
             if (error) {
                 console.log('SELECT Token query error:');
                 console.log(error);
