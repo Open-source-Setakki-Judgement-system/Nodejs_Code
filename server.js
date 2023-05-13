1819be434f95928c30b75d2e1bf7da0390a13910
phoenix9469 <phoenix9469@kakao.com>
2023-05-13 21:47:48 +0900

    device type 데이터 추가

--- server.js
+++ server.js
@@ -260,7 +260,7 @@ application.on('connection', socket => {
             console.log(results);
         });
 
-        connection.query(`SELECT device_id FROM PushAlert WHERE Token = ? ORDER BY device_id;`, [token], function (error, results) {
+        connection.query(`SELECT device_id, device_type FROM PushAlert WHERE Token = ? ORDER BY device_id;`, [token], function (error, results) {
             if (error) {
                 console.log('SELECT Token query error:');
                 console.log(error);
