c0e943bbfe18cee76390d2425e6289ef66e66101
phoenix9469 <phoenix9469@kakao.com>
2024-02-05 21:39:02 +0900

    Websocket으로 변경

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const credential = require('./var.js');
+const serAccount = require('./firebase_token.json')
 const express = require('express')
 const cors = require('cors')
 const mysql = require('mysql');
@@ -43,13 +44,27 @@ const options = {
 
 const https = require('https').createServer(options, app);
 const http = require('http').createServer(app)
-const io = require('socket.io')(https)
-const application = io.of('/application');
-
-const serAccount = require('./firebase_token.json')
 
 const http_port = 80
 const https_port = 443
+
+const wsModule = require('ws');
+
+const ClientSocket = new wsModule.Server(
+    {
+        noServer: true
+    }
+);
+
+const DeviceSocket = new wsModule.Server(
+    {
+        noServer: true
+    }
+);
+
+function heartbeat() {
+    this.isAlive = true;
+}
 
 fcm.initializeApp({
     credential: fcm.credential.cert(serAccount),
@@ -85,6 +100,69 @@ client.on('ready', (c) => {
 });
 
 client.login(credential.discord_token);
+
+app.on('upgrade', function upgrade(request, socket, head) {
+    const { pathname } = url.parse(request.url);
+    const user = auth(request);
+    if (pathname === '/client') {
+        ClientSocket.handleUpgrade(request, socket, head, function done(ws) {
+            ClientSocket.emit('connection', ws, request);
+        });
+    } else if(pathname === '/device')
+    {
+        if (!user || user.name !== credential.auth_name || user.pass !== credential.auth_pw) {
+            socket.destroy();
+        } else {
+            console.log(request.headers)
+            console.log(`[Device][Connected] [${request.headers['sec-websocket-key']},${request.headers['hwid']},${request.headers['ch1']},${request.headers['ch2']}]`);
+            DeviceSocket.handleUpgrade(request, socket, head, function done(ws) {
+                DeviceSocket.emit('connection', ws, request);
+            });
+        }
+    }
+    else {
+        socket.destroy();
+    }
+});
+
+ClientSocket.on('connection', (ws, request) => {
+    console.log(`Client Connected`);
+    if (ws.readyState === ws.OPEN) {
+        ws.send(`Hello From Server`);
+    }
+    ws.on('close', () => {
+        //console.log(`Websocket[${request.headers['sec-websocket-key']}] closed`);
+    })
+});
+
+DeviceSocket.on('connection', (ws, request) => {
+    //console.log(request.headers);
+    ws.isAlive = true;
+    ws.on('pong', heartbeat);
+    if (ws.readyState === ws.OPEN) {
+        ws.send(`Hello From Server`);
+    }
+
+    ws.on('message', (msg) => {
+        console.log(`Message Received : ${msg}`);
+        ClientSocket.clients.forEach(function (client) {
+            client.send(msg.toString());
+        });
+    })
+
+    ws.on('close', () => {
+        console.log(`[Device] [${request.headers['sec-websocket-key']}] Closed.`);
+    })
+});
+
+const interval = setInterval(function ping() {
+    DeviceSocket.clients.forEach(function each(ws) {
+        if (ws.isAlive === false) return ws.terminate();
+        console.log("ping");
+        ws.isAlive = false;
+        ws.ping();
+    });
+}, 10000);
 
 app.get('/', (req, res) => {
     res.sendStatus(200)
@@ -183,155 +261,31 @@ app.post("/push_cancel", (req, res) => {//알림 취소 기능
 
 });
 
-schedule.scheduleJob("*/10 * * * *", () => {
-    connection.query(`SELECT id,heartbeat from deviceStatus;`, (error, results) => {
-        if (error) {
-            console.log('deviceStatus Select query error:');
-            console.log(error);
-            return;
-        }
-        //console.log(results);
-        for (let i = 0; i < results.length; i++) {
-            if (moment(moment().format()).diff(results[i].heartbeat, 'minutes') > 10) {
-                connection.query(`UPDATE deviceStatus SET state = 2 WHERE id = ?;`, [results[i].id], (error, results) => {
-                    if (error) {
-                        console.log('deviceStatus Update query error:');
-                        console.log(error);
-                        return;
-                    }
-                });
-                //console.log(results[i].id + "is dead")
-            }
-        }
-    });
-})
-
 //Socket.io-Gateway
 
-io.on('connection', socket => {
-    console.log('[Socket.IO] Embedded device Connected:', socket.id)
+// io.on('connection', socket => {
+//     console.log('[Socket.IO] Embedded device Connected:', socket.id)
 
-    //Gateway에서 update_state 받으면
-    socket.on('update_state', state_data => {
-        const { id, state, alive } = state_data;
-        console.log("[Device] ID: " + id + " Status: " + state)
-        connection.query(`UPDATE deviceStatus SET heartbeat = ? WHERE id = ?;`, [moment().format(), id], (error, results) => {
-            if (error) {
-                console.log('deviceStatus Update query error:');
-                console.log(error);
-                return;
-            }
-            //console.log(results);
-        });
-        if (state != '2') {//생존신호
-            StatusUpdate(id,state)
-        }
-    })
-    socket.on('disconnect', function() {
-        console.log('[Socket.IO] Embedded device Disconnected:', socket.id)
-    })
-})
-
-//Socket.io-Application,Frontend
-
-application.on('connection', socket => {
-    console.log('[Socket.IO] Client Connected:', socket.id)
-
-    //Application과 Frontend에 현재 상태 DB 넘기기
-    connection.query(`SELECT * FROM deviceStatus;`, function (error, results) {
-        if (error) {
-            console.log('SELECT * FROM deviceStatus query error:');
-            console.log(error);
-            return;
-        }
-        application.to(socket.id).emit('update', results);
-        //console.log('==============================================')
-    });
-
-    //Application에서 request_push 받으면
-    socket.on('request_push', push_data => {
-        const { token, device_id, expect_state } = push_data;
-        console.log("[App] Push Request Device Token: " + token + " Device ID: " + device_id + " Expect Status: " + expect_state)
-
-        //DB에 중복되는 값 있는지 확인
-        connection.query(`SELECT Token FROM PushAlert WHERE device_id = ? AND Expect_Status = ? AND Token = ?;`, [device_id, expect_state, token], function (error, results) {
-            let type = new Array();
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-
-            //중복이면 return
-            if (results.length > 0) {
-                console.log('This is a duplicate value');
-                console.log('==============================================')
-                return;
-            } else {//중복 아니면 DB에 Token 등록
-                connection.query(`SELECT device_type FROM deviceStatus WHERE id = ?;`, [device_id], function (error, type_results) {
-                    if (error) {
-                        console.log('SELECT device_type query error:');
-                        console.log(error);
-                        return;
-                    }
-
-                    connection.query(`INSERT INTO PushAlert (Token, device_id, Expect_Status, device_type) VALUES (?, ?, ?, ?);`, [token, device_id, expect_state, type_results[0].device_type], (error, results) => {
-                        if (error) {
-                            console.log('deviceStatus Update query error:');
-                            console.log(error);
-                            return;
-                        }
-                        //console.log(results);
-                    });
-                });
-
-            }
-        });
-    })
-    //Application에서 푸시 신청 목록 보기 누르면
-    socket.on('view_request', view_requset_data => {
-        const { token } = view_requset_data;
-        console.log("[App] Push List Request Device Token: " + token)
-
-        connection.query(`SELECT device_id, device_type, state FROM PushAlert WHERE Token = ? ORDER BY device_id;`, [token], function (error, results) {
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-            //console.log("socket.io 'request_list' sent");
-            //socket.emit('request_list', results);
-            application.to(socket.id).emit('request_list', results);
-            //console.log(results);
-        });
-    })
-    //Application에서 푸시 신청 제거하면
-    socket.on('remove_request', remove_request_data => {
-        const { token, device_id } = remove_request_data;
-        console.log("[App] Push Cancel Request Device Token: " + token + " Device ID: " + device_id)
-
-        connection.query(`DELETE FROM PushAlert WHERE device_id = ? AND Token = ?;`, [device_id, token], function (error, results) {
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-            //console.log(results);
-        });
-
-        connection.query(`SELECT device_id, device_type FROM PushAlert WHERE Token = ? ORDER BY device_id;`, [token], function (error, results) {
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-            //console.log("socket.io 'request_list' sent");
-            //socket.emit('request_list', results);
-            application.to(socket.id).emit('request_list', results);
-            //console.log(results);
-        });
-    })
-})
+//     //Gateway에서 update_state 받으면
+//     socket.on('update_state', state_data => {
+//         const { id, state, alive } = state_data;
+//         console.log("[Device] ID: " + id + " Status: " + state)
+//         connection.query(`UPDATE deviceStatus SET heartbeat = ? WHERE id = ?;`, [moment().format(), id], (error, results) => {
+//             if (error) {
+//                 console.log('deviceStatus Update query error:');
+//                 console.log(error);
+//                 return;
+//             }
+//             //console.log(results);
+//         });
+//         if (state != '2') {//생존신호
+//             StatusUpdate(id,state)
+//         }
+//     })
+//     socket.on('disconnect', function() {
+//         console.log('[Socket.IO] Embedded device Disconnected:', socket.id)
+//     })
+// })
 
 http.listen(http_port, () => {
     console.log(`Listening to port ${http_port}`)
