66fe62cf1d6706fd7e3c79b6c7df218380d57c7f
phoenix9469 <phoenix9469@kakao.com>
2024-03-11 10:48:55 +0900

    Socket.IO 코드 삭제

--- server.js
+++ server.js
@@ -46,9 +46,6 @@ const options = {
 const https = require('https').createServer(options, app);
 const http = require('http').createServer(app)
 
-const io = require('socket.io')(https)
-const application = io.of('/application');
-
 const http_port = 80
 const https_port = 443
 
@@ -480,109 +477,6 @@ app.post("/push_cancel", (req, res) => {//알림 취소 기능
 
 });
 
-io.on('connection', socket => {
-    console.log('[Socket.IO] Embedded device Connected:', socket.id)
-})
-
-application.on('connection', socket => {
-    console.log('[Socket.IO] Client Connected:', socket.id)
-
-    //Application과 Frontend에 현재 상태 DB 넘기기
-    connection.query(`SELECT * FROM deviceStatus;`, function (error, results) {
-        if (error) {
-            console.log('SELECT * FROM deviceStatus query error:');
-            console.log(error);
-            return;
-        }
-        application.to(socket.id).emit('update', results);
-        //console.log('==============================================')
-    });
-
-    //Application에서 request_push 받으면
-    socket.on('request_push', push_data => {
-        const { token, device_id, expect_state } = push_data;
-        console.log("[App] Push Request Device Token: " + token + " Device ID: " + device_id + " Expect Status: " + expect_state)
-
-        //DB에 중복되는 값 있는지 확인
-        connection.query(`SELECT Token FROM PushAlert WHERE device_id = ? AND Expect_Status = ? AND Token = ?;`, [device_id, expect_state, token], function (error, results) {
-            let type = new Array();
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-
-            //중복이면 return
-            if (results.length > 0) {
-                console.log('This is a duplicate value');
-                console.log('==============================================')
-                return;
-            } else {//중복 아니면 DB에 Token 등록
-                connection.query(`SELECT device_type FROM deviceStatus WHERE id = ?;`, [device_id], function (error, type_results) {
-                    if (error) {
-                        console.log('SELECT device_type query error:');
-                        console.log(error);
-                        return;
-                    }
-
-                    connection.query(`INSERT INTO PushAlert (Token, device_id, Expect_Status, device_type) VALUES (?, ?, ?, ?);`, [token, device_id, expect_state, type_results[0].device_type], (error, results) => {
-                        if (error) {
-                            console.log('deviceStatus Update query error:');
-                            console.log(error);
-                            return;
-                        }
-                        //console.log(results);
-                    });
-                });
-
-            }
-        });
-    })
-    //Application에서 푸시 신청 목록 보기 누르면
-    socket.on('view_request', view_requset_data => {
-        const { token } = view_requset_data;
-        console.log("[App] Push List Request Device Token: " + token)
-
-        connection.query(`SELECT device_id, device_type, state FROM PushAlert WHERE Token = ? ORDER BY device_id;`, [token], function (error, results) {
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-            //console.log("socket.io 'request_list' sent");
-            //socket.emit('request_list', results);
-            application.to(socket.id).emit('request_list', results);
-            //console.log(results);
-        });
-    })
-    //Application에서 푸시 신청 제거하면
-    socket.on('remove_request', remove_request_data => {
-        const { token, device_id } = remove_request_data;
-        console.log("[App] Push Cancel Request Device Token: " + token + " Device ID: " + device_id)
-
-        connection.query(`DELETE FROM PushAlert WHERE device_id = ? AND Token = ?;`, [device_id, token], function (error, results) {
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-            //console.log(results);
-        });
-
-        connection.query(`SELECT device_id, device_type FROM PushAlert WHERE Token = ? ORDER BY device_id;`, [token], function (error, results) {
-            if (error) {
-                console.log('SELECT Token query error:');
-                console.log(error);
-                return;
-            }
-            //console.log("socket.io 'request_list' sent");
-            //socket.emit('request_list', results);
-            application.to(socket.id).emit('request_list', results);
-            //console.log(results);
-        });
-    })
-})
-
 http.listen(http_port, () => {
     console.log(`Listening to port ${http_port}`)
 })
@@ -639,7 +533,6 @@ function StatusUpdate(id, state) {
             console.log(error);
             return;
         }
-        application.emit('update', results)
         ClientSocket.clients.forEach(function (client) {
             client.send(JSON.stringify(results[0]));
         });
