172463ef0dd90a29ee9bc96064f87f0eadbaf918
phoenix9469 <phoenix9469@kakao.com>
2024-05-19 18:56:59 +0900

    Update 수정

--- server.js
+++ server.js
@@ -661,7 +661,7 @@ function StatusUpdate(id, state, type) {
             channel.send(`[${moment().format('HH:mm:ss')}] ${id}번 ${type_string}의 상태가 "${device_status_str}"으로 변경되었습니다.`);
         }
         //기기상태 DB 업데이트
-        connection.query(`UPDATE deviceStatus, boy_device SET state = ?, prev_state = ? WHERE id = ?;`, [state, state, id], (error, results) => {
+        connection.query(`UPDATE deviceStatus SET state = ?, prev_state = ? WHERE id = ?;`, [state, state, id], (error, results) => {
             if (error) {
                 console.log('deviceStatus Update query error:');
                 console.log(error);
@@ -693,7 +693,7 @@ function StatusUpdate(id, state, type) {
 
         if (state == 0 && type == 1)//ON
         {
-            connection.query(`UPDATE deviceStatus, boy_device SET ON_time = ? WHERE id = ?;`, [moment().format(), id], (error, results) => {
+            connection.query(`UPDATE deviceStatus SET ON_time = ? WHERE id = ?;`, [moment().format(), id], (error, results) => {
                 if (error) {
                     console.log('deviceStatus Update query error:');
                     console.log(error);
@@ -702,7 +702,7 @@ function StatusUpdate(id, state, type) {
                 //console.log(results);
             });
         } else if (state == 1 && type == 1) {//OFF
-            connection.query(`UPDATE deviceStatus, boy_device SET OFF_time = ? WHERE id = ?;`, [moment().format(), id], (error, results) => {
+            connection.query(`UPDATE deviceStatus SET OFF_time = ? WHERE id = ?;`, [moment().format(), id], (error, results) => {
                 if (error) {
                     console.log('deviceStatus Update query error:');
                     console.log(error);
